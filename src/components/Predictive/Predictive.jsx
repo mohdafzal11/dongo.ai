709fef0c09a506657cbf65f7507d5f83e20a51ba
Mohd Afzal <afzalnaved0000@gmail.com>
2024-06-28 12:09:48 +0530

    responsiveness added for predictive page

--- src/components/Predictive/Predictive.jsx
+++ src/components/Predictive/Predictive.jsx
@@ -3,13 +3,13 @@ import chat from "../../assets/chat.png";
 
 const Predictive = () => {
   return (
-    <div className=" flex  mx-20">
-      <div className="w-1/2 ">
+    <div className=" flex flex-col lg:flex-row mx-12 lg:mx-36">
+      <div className="w-full lg:w-1/2 ">
         <div className="flex flex-col justify-center  w-full h-full">
-          <h1 className="text-6xl text-white w-[80%]">
+          <h1 className=" text-4xl  lg:text-6xl text-white text-center lg:text-left lg:w-[90%]">
             Predictive Market Insights
           </h1>
-          <div className="w-[70%] my-8">
+          <div className="text-center lg:text-left lg:w-[70%] my-4 lg:my-8">
             <p className="text-xl text-[#A1AEE5]">
               Anticipate market trends with Dongo's Predictive Market Insights.
               By processing historical and current market data, our AI provides
@@ -20,7 +20,7 @@ const Predictive = () => {
           </div>
         </div>
       </div>
-      <div className="w-[40%] flex mx-auto">
+      <div className="lg:w-[40%] flex mx-auto  my-12 lg:my-0">
         <img decoding="async" src={chat} alt="" loading="lazy" />
       </div>
     </div>

e79c625992e1c1816756b17cc8fdd1fcb702c9bf
Mohd Afzal <afzalnaved0000@gmail.com>
2024-07-01 17:31:56 +0530

    aaa

--- src/components/Tokenomics/Tokenomics.jsx
+++ src/components/Tokenomics/Tokenomics.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "../../index.css";
+import logo from "../../assets/logo.png";
 
 const Tokenomics = () => {
   return (
@@ -18,6 +20,70 @@ const Tokenomics = () => {
             and revenue shares, ensuring your role in our innovative and
             successful future.
           </p>
+        </div>
+
+        <div>
+        {/* <svg xmlns="http://www.w3.org/2000/svg" style="width: 100%; height: 100%" viewBox="0 0 134 48" preserveAspectRatio="none" width="100%" height="100%"></svg> */}
+        </div>
+
+        <div className="flex text-white items-center justify-center">
+          <div className="space-y-4 w-[33%]">
+            <div className="flex  items-center space-x-2">
+              <div className="text-lg font-bold"> Max Supply</div>
+              <div className="text-base text-gray-400"> 1,000,000,000</div>
+            </div>
+
+            <div className="flex  items-center space-x-2">
+              <div className="text-lg font-bold">Symbol</div>
+              <div className="text-base text-gray-400">$DONGO</div>
+            </div>
+
+            <div className="flex  items-center space-x-2">
+              <div className="text-lg font-bold">Network</div>
+              <div className="text-base text-gray-400">ETH (ERC-20)</div>
+            </div>
+            <div className="flex  items-center space-x-2">
+              <div className="text-lg font-bold">Buy/Sell Tax</div>
+              <div className="text-base text-gray-400">4% / 4%</div>
+            </div>
+            <div className="flex flex-col   space-y-1">
+              <div className="text-lg font-bold">Contract Address</div>
+              <div className="text-base text-gray-400 ">
+                <p>0x8c213AE332274e6314Bf4</p>
+                <p> cF989604e7f61162967</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex w-[33%]">
+            <div className="flex border p-10 rounded-full">
+              <div className="border p-10  rounded-full">
+                <img src={logo} alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className="space-y-4 w-[33%]">
+            <div className="border py-3 px-5 rounded-lg space-y-1">
+              <h1 className="text-lg font-bold text-white">Supply</h1>
+              <p className="text-base text-gray-400">
+                12.5% Team & Seed Round (locked)
+              </p>
+              <p className="text-base text-gray-400">7.5% Marketing</p>
+              <p className="text-base text-gray-400">80% Liquidity</p>
+            </div>
+
+            <div className="border py-3 px-5 rounded-lg space-y-1">
+              <h1 className="text-lg font-bold text-white">
+                Buy/Sell Tax Allocations
+              </h1>
+              <p className="text-base text-gray-400">
+                Marketing and User Acquisition
+              </p>
+              <p className="text-base text-gray-400">Dongo Development Lab</p>
+              <p className="text-base text-gray-400">Team & Staking Pool</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
